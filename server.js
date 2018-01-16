707f490b434fd61327277767df7bf73695415c77
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-01-16 12:24:28 -0500

    set up Heroku for Express and React.

--- server.js
+++ server.js
@@ -6,25 +6,25 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URI, {
-    useMongoClient: true
-})
+app.use(express.static(__dirname + '/client/build/'));
+
+mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true})
 const connection = mongoose.connection;
 connection.on('connected', () => {
-  console.log('Mongoose Connected Successfully');    
-}); 
+    console.log('Mongoose Connected Successfully');
+});
 
 // If the connection throws an error
 connection.on('error', (err) => {
-  console.log('Mongoose default connection error: ' + err);
-}); 
+    console.log('Mongoose default connection error: ' + err);
+});
 
 app.use(bodyParser.json());
-app.get('/', (req,res) => {
-  res.send('Hello world!')
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/client/build/index.html')
 })
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log("Magic happening on port " + PORT);
+    console.log("Magic happening on port " + PORT);
 })
